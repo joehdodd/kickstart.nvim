07c54d14de958424a5aaf1c50fb15e2e670be243
Joe Dodd <joehdodd@gmail.com>
2024-01-09 13:11:41 -0500

    Update plugins.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,10 +2,6 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrwSettings = 1
-vim.g.loaded_netrwFileHandlers = 1
 return { {
     'navarasu/onedark.nvim',
     lazy = false,
@@ -32,6 +28,41 @@ return { {
         end,
         desc = 'Toggle Color Scheme'
     } }
+}, {
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    lazy = false,
+    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons', 'MunifTanjim/nui.nvim' },
+    keys = { {
+        '<leader>fe',
+        '<cmd>Neotree toggle<cr>',
+        desc = 'File Explorer - Toggle'
+    } },
+    config = function()
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+        vim.g.loaded_netrwSettings = 1
+        vim.g.loaded_netrwFileHandlers = 1
+        require('neo-tree').setup {
+            window = {
+                width = 30,
+                position = "right",
+                mappings = {
+                    ["<space>"] = "none"
+                }
+            },
+            filesystem = {
+                filtered_items = {
+                    visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+                    hide_dotfiles = false,
+                    hide_gitignored = true
+                },
+                follow_current_file = {
+                    enabled = true -- This will find and focus the file in the active buffer every time
+                }
+            }
+        }
+    end
 }, {
     'akinsho/toggleterm.nvim',
     version = "*",
@@ -63,35 +94,16 @@ return { {
         wk.register(opts.defaults)
     end,
 }, {
-    'nvim-neo-tree/neo-tree.nvim',
-    version = '*',
-    lazy = false,
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons', 'MunifTanjim/nui.nvim' },
-    keys = { {
-        '<leader>fe',
-        '<cmd>Neotree toggle<cr>',
-        desc = 'File Explorer - Toggle'
-    } },
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
-        require('neo-tree').setup {
-            window = {
-                width = 30,
-                position = "right",
-                mappings = {
-                    ["<space>"] = "none"
-                }
-            },
-            filesystem = {
-                filtered_items = {
-                    visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
-                    hide_dotfiles = false,
-                    hide_gitignored = true
-                },
-                follow_current_file = {
-                    enabled = true -- This will find and focus the file in the active buffer every time
-                }
+        require("bufferline").setup({
+            options = {
+                diagnostics = "nvim_lsp"
             }
-        }
+        })
+        vim.opt.termguicolors = true
     end
 }, {
     "zbirenbaum/copilot.lua",
@@ -108,105 +120,4 @@ return { {
             }
         })
     end
-}, {
-    'akinsho/bufferline.nvim',
-    version = "*",
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    config = function()
-        require("bufferline").setup({
-            options = {
-                diagnostics = "nvim_lsp"
-            }
-        })
-        vim.opt.termguicolors = true
-    end
-}, {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-        lsp = {
-            override = {
-                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                ["vim.lsp.util.stylize_markdown"] = true,
-                ["cmp.entry.get_documentation"] = true
-            }
-        },
-        routes = { {
-            filter = {
-                event = "msg_show",
-                any = { {
-                    find = "%d+L, %d+B"
-                }, {
-                    find = "; after #%d+"
-                }, {
-                    find = "; before #%d+"
-                } }
-            },
-            view = "mini"
-        } },
-        presets = {
-            bottom_search = true,
-            command_palette = true,
-            long_message_to_split = true,
-            inc_rename = true
-        }
-    },
-    keys = { {
-        "<S-Enter>",
-        function()
-            require("noice").redirect(vim.fn.getcmdline())
-        end,
-        mode = "c",
-        desc = "Redirect Cmdline"
-    }, {
-        "<leader>snl",
-        function()
-            require("noice").cmd("last")
-        end,
-        desc = "Noice Last Message"
-    }, {
-        "<leader>snh",
-        function()
-            require("noice").cmd("history")
-        end,
-        desc = "Noice History"
-    }, {
-        "<leader>sna",
-        function()
-            require("noice").cmd("all")
-        end,
-        desc = "Noice All"
-    }, {
-        "<leader>snd",
-        function()
-            require("noice").cmd("dismiss")
-        end,
-        desc = "Dismiss All"
-    }, {
-        "<c-f>",
-        function()
-            if not require("noice.lsp").scroll(4) then
-                return "<c-f>"
-            end
-        end,
-        silent = true,
-        expr = true,
-        desc = "Scroll forward",
-        mode = { "i", "n", "s" }
-    }, {
-        "<c-b>",
-        function()
-            if not require("noice.lsp").scroll(-4) then
-                return "<c-b>"
-            end
-        end,
-        silent = true,
-        expr = true,
-        desc = "Scroll backward",
-        mode = { "i", "n", "s" }
-    } },
-    dependencies = {
-        "MunifTanjim/nui.nvim",
-        "rcarriga/nvim-notify"
-    }
 } }
