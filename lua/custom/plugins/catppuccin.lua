d13f975580f2670bcbb0dea8348f810c262feb09
Joe Dodd <joehdodd@gmail.com>
2024-01-30 14:21:43 -0500

    Updates catppuccin theme config.

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -1,22 +1,23 @@
 return {
-    "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
-    config = function()
-        require("catppuccin").setup({
-            flavor = "macchiato",
-            styles = {
-                keywords = { "bold", "italic" },
-            },
-            color_overrides = {
-                latte = {
-                    rosewater = "#ca3c3a",
-
-                },
-            }
-
-        })
-        vim.cmd("colorscheme catppuccin-macchiato")
-    end,
-    keys = { { "<leader>tcd", ":Catppuccin macchiato<CR>", desc = "Catppuccin Macchiato" }, { "<leader>tcl", ":Catppuccin latte<CR>", desc = "Catppuccin Latte" } },
+  'catppuccin/nvim',
+  lazy = true,
+  name = 'catppuccin',
+  config = function()
+    require('catppuccin').setup {
+      flavor = 'macchiato',
+      styles = {
+        keywords = { 'italic' },
+      },
+      color_overrides = {
+        latte = {
+          rosewater = '#ca3c3a',
+        },
+      },
+    }
+    vim.cmd 'colorscheme catppuccin-macchiato'
+  end,
+  keys = {
+    { '<leader>tcd', ':Catppuccin macchiato<CR>', desc = 'Catppuccin Macchiato' },
+    { '<leader>tcl', ':Catppuccin latte<CR>', desc = 'Catppuccin Latte' },
+  },
 }
