4793b6b602ab75bebc3ee7fc36f1d33e0c2cac0d
Joe Dodd <joehdodd@gmail.com>
2024-02-08 13:53:05 -0500

    Updates tokyonight. Adds theme.lua. Doesn't work

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,5 +1,5 @@
 -- Themery block
 -- This block will be replaced by Themery.
-vim.cmd("colorscheme catppuccin-latte")
-vim.g.theme_id = 2
+vim.cmd("colorscheme catppuccin-macchiato")
+vim.g.theme_id = 1
 -- end themery block
