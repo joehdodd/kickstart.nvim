bbff8e0b92752c9b8aeae49bf4117324c476746f
Joe Dodd <joehdodd@gmail.com>
2024-03-06 09:32:32 -0500

    Update config.

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,14 +1,11 @@
 return {
-    "zbirenbaum/copilot.lua",
-    config = function()
-        require("copilot").setup({
-            suggestion = {
-                auto_trigger = true,
-                debounce = 25,
-                keymap = {
-                    accept = "<C-l>",
-                },
-            }
-        })
-    end
+  'zbirenbaum/copilot.lua',
+  config = function()
+    require('copilot').setup {
+      suggestion = {
+        auto_trigger = true,
+        debounce = 25,
+      },
+    }
+  end,
 }
