171837f0824e5b65d5b19c2af8dbbee218896b3e
Joe Dodd <joehdodd@gmail.com>
2024-10-25 09:40:42 -0400

    Updates plugins.

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -5,17 +5,17 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
-    '3rd/image.nvim',              -- Optional image support in preview window: See `# Preview Mode` for more information
+    '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
-    require('neo-tree').setup({
+    require('neo-tree').setup {
       window = {
         width = 32,
-      }
-    })
+      },
+    }
   end,
   keys = { {
-    '<leader>te',
+    '\\',
     '<cmd>Neotree toggle<cr>',
     desc = 'File [e]xplorer',
   } },
