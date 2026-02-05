72f91f8ba1fbf35dfaeebc7fa6d28334167e1034
Joe Dodd <joehdodd@gmail.com>
2026-02-05 11:38:12 -0500

    Updates to plugins.

---
+++ lua/custom/plugins/agentic.lua
@@ -0,0 +1,25 @@
+return {
+  'carlos-algms/agentic.nvim',
+  opts = {
+    provider = 'opencode-acp', -- or gemini-acp, codex-acp, opencode-acp
+    debug = true,
+  },
+  keys = {
+    {
+      '<C-A>',
+      function()
+        require('agentic').toggle()
+      end,
+      desc = 'Agentic Open',
+      mode = { 'n', 'v', 'i' },
+    },
+    {
+      "<C-'>",
+      function()
+        require('agentic').add_selection_or_file_to_context()
+      end,
+      desc = 'Agentic add selection or current file to context',
+      mode = { 'n', 'v' },
+    },
+  },
+}
