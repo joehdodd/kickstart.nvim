59503f175d450563e43b54e6c0156198cd5dc770
Joe Dodd <joehdodd@gmail.com>
2026-09-14 13:24:53 -0400

    Updates to plugins.

--- init.lua
+++ init.lua
@@ -387,7 +387,11 @@ require('lazy').setup({
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
-        -- pickers = {}
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
@@ -681,22 +685,6 @@ require('lazy').setup({
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        local lsp_format_opt
-        if disable_filetypes[vim.bo[bufnr].filetype] then
-          lsp_format_opt = 'never'
-        else
-          lsp_format_opt = 'fallback'
-        end
-        return {
-          timeout_ms = 500,
-          lsp_format = lsp_format_opt,
-        }
-      end,
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
--- lua/custom/plugins/telescope-file-browser.lua
+++ lua/custom/plugins/telescope-file-browser.lua
@@ -1,21 +1,26 @@
 return {
-    "nvim-telescope/telescope-file-browser.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-    init = function()
-        local fb_actions = require "telescope".extensions.file_browser.actions
-        require("telescope").setup {
-            extensions = {
-                file_browser = {
-                    hijack_netrw = true,
-                    path_to_cwd = true,
-                },
-            },
-        }
-        require("telescope").load_extension "file_browser"
-    end,
-    keys = { {
-        '<leader>sb',
-        '<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>',
-        desc = '[B]rowse Files'
-    } },
+  'nvim-telescope/telescope-file-browser.nvim',
+  dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+  init = function()
+    local fb_actions = require('telescope').extensions.file_browser.actions
+    require('telescope').setup {
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
+      },
+      extensions = {
+        file_browser = {
+          hijack_netrw = true,
+          path_to_cwd = true,
+        },
+      },
+    }
+    require('telescope').load_extension 'file_browser'
+  end,
+  keys = { {
+    '<leader>fb',
+    '<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>',
+    desc = '[B]rowse Files',
+  } },
 }
